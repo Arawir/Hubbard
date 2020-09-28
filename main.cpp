dd6a5d45921c331360ec2a129c19a0e9de55128c
Arawir <mmarciniak87@wp.pl>
2020-09-28 13:21:25 +0200

    prepare script

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ void tdvpStepWithBasisExtensionIfNeeded(MPS &psi, MPO &H, double dTime, Sweeps &
        std::vector<Real> epsilonK = {getD("cutoff"),getD("cutoff"),getD("cutoff")};
        addBasis(psi,H,epsilonK,{"Cutoff",getD("cutoff"),"Method","DensityMatrix","KrylovOrd",4,"DoNormalize",true,"Quiet",true});
     }
-    tdvp(psi,H,im*dTime,sweeps,{"DoNormalize",true,"Quiet",true,"NumCenter",2});
+    tdvp(psi,H,im*dTime/sweeps.nsweep(),sweeps,{"DoNormalize",true,"Quiet",true,"NumCenter",2});
 }
 
 int main(int argc, char *argv[])
@@ -54,16 +54,42 @@ int main(int argc, char *argv[])
         }
     };
 
+    Experiments("timeEvGs") = [](){
+        auto [sites,psi,H,sweeps] = prepareExpBasic();
+        ExpCon.setSites(sites);ExpCon("E") = H;
+
+        ExpCon.addPoint("Prepare operator");
+        auto ampo = AutoMPO(sites);
+        ampo += 1,"S+",getI("L")/2;
+        MPO OpSpinPlus = toMPO(ampo);
+
+        ExpCon.addPoint("Starting DMRG");
+        dmrg(psi,H,sweeps);
+        ExpCon.calc(psi,oMode::b,"rtime","mem","dim","E","Ekin","Epot","N","Nd","Sz1:L","N1:L","Nd1:L");
+
+        ExpCon.addPoint("Apply operator");
+        auto psi2 = removeQNs(psi);
+        auto psi3 = applyMPO(OpSpinPlus,psi2,{"Method=","DensityMatrix","MaxDim=",100,"Cutoff=",1E-20,"Normalize=",true});
+        psi3.noPrime();
+
+        ExpCon.addPoint("Starting TDVP");
+
+        for(double time=0; time<=getD("maxtime")+getD("dtime")+0.001; time+=getD("dtime")){
+            ExpCon.calc(psi3,oMode::b,"t:",time,"rtime","mem","dim","E","Ekin","Epot","N","Nd","Sz1:L","N1:L","N21:L","Nd1:L");
+            tdvpStepWithBasisExtensionIfNeeded(psi3,H,getD("dtime"),sweeps);
+        }
+    };
 
 
-    Params.add("t","double","0.5");
-    Params.add("U","double","2.1");
+
+    Params.add("t","double","1");
+    Params.add("U","double","1");
 
     Params.add("L","int","24");
     Params.add("PBC","bool","0");
 
     Params.add("dtime","double","0.1");
-    Params.add("maxtime","double","96.0");
+    Params.add("maxtime","double","196.0");
 
     Params.add("Silent","bool","1");
     Params.add("cutoff","double","1E-8");
@@ -71,10 +97,10 @@ int main(int argc, char *argv[])
     Params.add("minDim","int","1");
     Params.add("maxDim","int","200");
     Params.add("niter","int","10");
-    Params.add("state","string","L/2*Up-L/1*Dn");
+    Params.add("state","string","Up-Dn");
     Params.add("ConserveNf","bool","1");
     Params.add("ConserveSz","bool","0");
-    Params.add("exp","string","timeEv2");
+    Params.add("exp","string","timeEvGs");
     Params.add("ConserveQNs","bool","0");
 
     Params.add("PBSenable","bool","0");
